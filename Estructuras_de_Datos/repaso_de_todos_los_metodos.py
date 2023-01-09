1a4ada1e992ec52a9c29a2c1933c442fa98083f4
Emill <lemito66@gmail.com>
2023-01-09 14:55:29 -0500

    por seleccion ascendente

--- Estructuras_de_Datos/repaso_de_todos_los_metodos.py
+++ Estructuras_de_Datos/repaso_de_todos_los_metodos.py
@@ -8,5 +8,16 @@ def metodo_burbuja(lista):
                 lista[j + 1] = numero_a_reemplazar
     return lista
 
-print(metodo_burbuja([1,78,99,100,4,-5]))
+def metodo_por_seleccion_ascendente(lista):
+    for i in range(len(lista)):
+        numero_minimo = i
+        for j in range(numero_minimo + 1, len(lista)):
+            if lista[j] < lista[numero_minimo]:
+                numero_minimo = j
+        numero_a_reemplazar = lista[i]
+        lista[i] = lista[numero_minimo]
+        lista[numero_minimo] = numero_a_reemplazar
+    return lista
 
+print(metodo_burbuja([1,78,99,100,4,-5]))
+print(metodo_por_seleccion_ascendente([1,78,99,100,4,-5]))
