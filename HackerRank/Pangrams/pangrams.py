afdbd2a1017624f31f37e5491178d0848446d7aa
Emill <lemito66@gmail.com>
2023-01-26 10:34:06 -0500

    listo

--- HackerRank/Pangrams/pangrams.py
+++ HackerRank/Pangrams/pangrams.py
@@ -1,5 +1,4 @@
 def pangrams(sentence: str):
-    result = ''
     new_sentence = sentence.lower()
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
                 'h', 'i', 'j', 'k', 'l', 'm', 'n',
@@ -12,56 +11,14 @@ def pangrams(sentence: str):
                 alphabet_dict[i] += 1
     else:
         return f'Your sentence have much length'
-    """ for i in alphabet_dict:
-        if i in alphabet_dict.values() == 0:
-            return f'Not paragram'
-        else:
-            pass
-            return f'Paragram, {alphabet_dict}' """
     for value in alphabet_dict.values():
         if value == 0:
-            return f'Not Pangram'
-            break
-        else:
-            return f'Pangram'
-
-    return alphabet_dict
+            return f'not pangram'
+        
+    return f'pangram'
 
 
-#print(pangrams('We promptly judged antique ivory buckles for the next prize'))
+
+print(pangrams('We promptly judged antique ivory buckles for the next prize'))
 #print(pangrams('We promptly judged antique ivory buckles for the prize'))
 
-""" alphabet_dict = {
-    'a': 0,
-    'b': 0,
-    'c': 0,
-    'd': 0,
-    'e': 0,
-    'f': 0,
-    'g': 0,
-    'h': 0,
-    'i': 0,
-    'j': 0,
-    'k': 0,
-    'l': 0,
-    'm': 0,
-    'n': 0,
-    'o': 0,
-    'p': 0,
-    'q': 0,
-    'r': 0,
-    's': 0,
-    't': 0,
-    'u': 0,
-    'v': 0,
-    'w': 0,
-    'x': 0,
-    'y': 0,
-    'z': 1
-}
-for key in alphabet_dict.values():
-    if key == 0:
-        print('Not Pangram')
-        break
-    else:
-        print('Pangram') """
